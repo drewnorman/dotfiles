8fb6d7c66f4556ca47c4f027601f63caf4e88b0c
Drew Norman <drewnorman@foxfuelcreative.com>
2021-05-04 18:18:36 -0500

    Adding install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,14 +1,14 @@
-git clone --bare git@github.com:drewnorman/dotfiles.git $HOME/.cfg
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+git clone --bare git@github.com:drewnorman/dotfiles.git $HOME/.dotfiles
+config() {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
+mkdir -p .dotfiles-backup
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out configuration.";
+  echo "Checked out dotfiles.";
   else
     echo "Backing up pre-existing dotfiles.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Dotfiles
 
 This repository houses my various application configurations.
+
+## Installation
+
+To set up configuration tracking in your $HOME directory, run:
+
+`curl -Lks https://raw.githubusercontent.com/drewnorman/dotfiles/master/.bin/install.sh | /bin/bash`
